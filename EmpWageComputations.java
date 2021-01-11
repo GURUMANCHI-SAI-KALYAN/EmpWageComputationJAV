3fea53f8193f3e4e4435b61774b163b723108501
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-11 16:14:33 +0530

    ADDED:monyhwageUC5 program added

--- EmpWageComputations.java
+++ EmpWageComputations.java
@@ -6,10 +6,12 @@ public class EmpWageComputations {
         final int IS_FULL_TIME = 1;
 	final int IS_PART_TIME = 2;
 	final int EMP_RATE_HR = 20 ;
+	final int NUM_WORKING_DAYS = 20;
 
 	int emphr = 0;
 	int empwage = 0;
-
+	int totempwage = 0;
+	for (int num = 0; num < NUM_WORKING_DAYS; num++) {
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
 	switch (empCheck) {
@@ -27,6 +29,10 @@ public class EmpWageComputations {
            	   break;
        }
 	empwage = emphr * EMP_RATE_HR;
-	System.out.println("Employee Wage :" + empwage);
+	totempwage += empwage;
     }
+	System.out.println("Employee Wage :" + totempwage);
+
+  }
+
 }
