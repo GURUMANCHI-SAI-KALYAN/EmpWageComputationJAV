6b58f85eaa1b94d7a324427e506781a342af80ed
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-11 15:34:50 +0530

    ADDED:AttandanceUC1 program added

--- EmpWageComputations.java
+++ EmpWageComputations.java
@@ -1,3 +1,17 @@
-public class EmpWageBuilder {
-	public static void main (string[] args) {
-	 systemp.out.println("welcome");
+public class EmpWageComputations {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wedge computation");
+
+        final int IS_FULL_TIME = 1;
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        System.out.println(empCheck);
+
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present.");
+        } else {
+            System.out.println("Employee is Absent.");
+        }
+    }
+}
